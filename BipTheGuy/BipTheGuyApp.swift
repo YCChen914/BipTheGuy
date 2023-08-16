066970e75d622780dac41cd37ed2d973e785b9da
PaulChen <csz890914@gmail.com>
2023-08-16 15:25:37 +0800

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by 陳永承 on 2023/8/16.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
