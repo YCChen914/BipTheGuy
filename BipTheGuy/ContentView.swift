850d1396cd8e6ea29a93fefd84686c5a36818cc2
PaulChen <csz890914@gmail.com>
2023-08-16 16:05:38 +0800

    course ch3.0 ~ ch3.2

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false // will immediately shrink using .scaleEffect to 90% of size
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)){
+                        animateImage = true // will go from 90% to 100% size but using the .spring animation
+                    }
+                }
+                
+            Spacer()
+            Button {
+                // TODO: Button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
+        
+    }
+    func playSound(soundName: String){
+        guard let soundFile = NSDataAsset(name: soundName)else{
+            print("😡Could not read file named \(soundName)")
+            return
+        }
+        do{
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        }catch{
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
